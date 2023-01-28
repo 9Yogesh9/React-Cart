07a6ade99757ad255abb50df7bc396cf2015848a
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-01-28 12:30:04 +0530

    increase and decrease funcitionality implemented

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,14 +11,24 @@ class CartItem extends React.Component {
         }
     }
     increaseQuantity = () => {
-        console.log(this);
+        // console.log(this.state.qty+1);
+        // form 1
+        // this.setState({ qty: this.state.qty + 1 })
+        // form 2 for setState
+        this.setState((prevState) => { return { qty: prevState.qty + 1 } })
+    }
+
+    decreaseQuantity = () => {
+        this.setState((prevState) => {
+            return { qty: prevState.qty - 1 < 0 ? 0 : prevState.qty - 1 }
+        })
     }
     render() {
         const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img style={styles.image} alt="" />
                 </div>
                 <div className="right-block">
                     <div style={{ fontSize: 25 }}>{title}</div>
@@ -30,11 +40,13 @@ class CartItem extends React.Component {
                             alt="increase"
                             className="action-icons"
                             onClick={this.increaseQuantity}
+                        // onClick={() => { this.setState({ qty: qty + 1 }) }}
                         />
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/9068/9068752.png"
                             alt="decrease"
                             className="action-icons"
+                            onClick={this.decreaseQuantity}
                         />
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/3178/3178290.png"
